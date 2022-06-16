796130238f6c08056d4bedafd5be7dc4fc862236
BlackHeart32 <eguzmandls@gmail.com>
2022-06-16 11:53:57 -0400

    1

--- students_list.js
+++ students_list.js
@@ -1,10 +1,12 @@
 const studentsList = document.getElementById("studentslist");
 const students = JSON.parse(localStorage.getItem("studentList")) || [];
+const studentInfo = document.getElementById("studentinfo");
 
-
+studentsArray = [];
 
 studentsList.innerHTML = students
 .map(student => {
+    studentsArray.push(student);
     return `<li class= "registered_student popup" onclick="showStudent()"> ${student.fName} ${student.lName}`;
 
 })
@@ -12,7 +14,9 @@ studentsList.innerHTML = students
 
 
 function showStudent() {
-    var popup = document.getElementById("studentpopup");
-    popup.classList.toggle("show");
+    const student = document.getElementsByClassName("registered_student");
+    const info = document.getElementById("studentinfo");
+    
+    info.innerHTML = `<p>${student}</p>`
     
 }; 
